84d6e6c112f72012f86a623857224d41bc9daece
Gpspidey19 <gianpaulcustodio1198@gmail.com>
2018-08-04 23:41:13 -0500

    final

---
+++ Snake/Snake/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.puntos = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(312, 604);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PUNTAJE:";
+            // 
+            // puntos
+            // 
+            this.puntos.AutoSize = true;
+            this.puntos.Font = new System.Drawing.Font("Lucida Sans", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puntos.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.puntos.Location = new System.Drawing.Point(519, 605);
+            this.puntos.Name = "puntos";
+            this.puntos.Size = new System.Drawing.Size(33, 32);
+            this.puntos.TabIndex = 1;
+            this.puntos.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.canvas.Location = new System.Drawing.Point(0, -1);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(930, 590);
+            this.canvas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.canvas.TabIndex = 2;
+            this.canvas.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(928, 649);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.puntos);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Gianpaul Custodio";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label puntos;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox canvas;
+    }
+}
+
---
+++ Snake/Snake/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        Cola cabeza;
+        Graphics g;
+        int xdir = 0, ydir = 0;
+        int cuadro = 10;
+        int puntaje = 0;
+        Boolean ejex = true, ejey = true;
+        Comida comida;
+        public Form1()
+        {
+            InitializeComponent();
+            cabeza = new Cola(10, 10);
+            comida = new Comida();
+            g = canvas.CreateGraphics();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            cabeza.dibujar(g);
+            comida.dibujar(g);
+            movimiento();
+            if(cabeza.interseccion(comida))
+            {
+                comida.colocar();
+                cabeza.meter();
+                puntaje++;
+                puntos.Text = puntaje.ToString();
+            }
+            choquedeljuego();
+            choquePared();
+        }
+        public void choquePared()
+        {
+            if(cabeza.verX()<0 || cabeza.verX() > ClientRectangle.Width || cabeza.verY()<0 || cabeza.verY()> canvas.ClientRectangle.Height)
+            {
+                findeljuego();
+            }
+        }
+        public void findeljuego()
+        {
+            puntaje = 0;
+            puntos.Text = "0";
+            ejex = true;
+            ejey = true;
+            xdir = 0;
+            ydir = 0;
+            cabeza = new Cola(10, 10);
+            comida = new Comida();
+            MessageBox.Show("GAME OVER");
+        }
+        public void choquedeljuego()
+        {
+            Cola temp;
+            try
+            {
+                temp = cabeza.versiguiente().versiguiente();
+            }
+            catch (Exception ex)
+            {
+                temp = null;
+            }
+            while(temp!= null)
+            {
+                if(cabeza.interseccion(temp))
+                {
+                    findeljuego();
+                }
+                else
+                {
+                    temp = temp.versiguiente();
+                }
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(ejex)
+            {
+                if (e.KeyCode == Keys.Up)
+                {
+                    ydir = -cuadro;
+                    xdir = 0;
+                    ejex = false;
+                    ejey = true;
+                }
+                if (e.KeyCode == Keys.Down)
+                {
+                    ydir = cuadro;
+                    xdir = 0;
+                    ejex = false;
+                    ejey = true;
+                }
+            }
+            if(ejey)
+            {
+                if(e.KeyCode == Keys.Right)
+                {
+                    ydir = 0;
+                    xdir = cuadro;
+                    ejex = true;
+                    ejey = false;
+                }
+                if (e.KeyCode == Keys.Left)
+                {
+                    ydir = 0;
+                    xdir = -cuadro;
+                    ejex = true;
+                    ejey = false;
+                }
+            }
+        }
+        public void movimiento()
+        {
+            cabeza.setxy(cabeza.verX() + xdir, cabeza.verY() + ydir);
+        }
+        
+    }
+}
